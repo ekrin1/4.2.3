f5315092c06af128de9f1554713befcaf80ff109
ekrin1 <kottovakatya@mail.ru>
2025-12-16 15:05:23 +0300

    f

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import App from './App.tsx'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { theme } from './theme.ts';
+import { MantineProvider } from "@mantine/core";
+
+createRoot(document.getElementById('root')!).render(
+<MantineProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+</MantineProvider>
+)
